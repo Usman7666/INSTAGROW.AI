7975e570cd8a82a45b862a7d2b8ffa3d5c0ea1d0
Muhammad Usman <usmanawan7666@gmail.com>
2025-05-23 05:45:57 -0700

    add media query in gsap

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -9,208 +8,415 @@ document.addEventListener("DOMContentLoaded", (event) => {
     stagger: 0.3,
   });
 
-  gsap.from(".imageslist", {
-    y: 20,
-    opacity: 0.6,
-    duration: 0.5,
-    stagger: 0.3,
-    scrollTrigger: {
-      trigger: ".imageslist",
-      start: "top 90%",
-      scroller: "body",
-     
-    },
+  const mm = gsap.matchMedia();
+
+  mm.add("(min-width: 768px)", () => {
+    gsap.from(".imageslist", {
+      y: 20,
+      opacity: 0.6,
+      duration: 0.5,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: ".imageslist",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
+
+    gsap.from(".heading", {
+      y: 200,
+      opacity: 0,
+      duration: 1,
+      scale: 0.2,
+      scrollTrigger: {
+        trigger: ".heading",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
+
+    gsap.from(".before-image", {
+      y: 50,
+      duration: 2,
+      delay: 0.1,
+      scrollTrigger: {
+        trigger: ".before-image",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
+
+    gsap.from(".image", {
+      opacity: 0,
+      duration: 2,
+      x: 500,
+      scrollTrigger: {
+        trigger: ".image",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".preciseimage", {
+      opacity: 0,
+      duration: 2,
+      x: 500,
+      scrollTrigger: {
+        trigger: ".preciseimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".precisecontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".precisecontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".analyticsimage", {
+      opacity: 0,
+      duration: 2,
+      x: -500,
+      scrollTrigger: {
+        trigger: ".analyticsimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".analyticscontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".analyticscontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".Integratedcontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".Integratedcontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".Aiimage", {
+      opacity: 0,
+      duration: 2,
+      x: 500,
+      scrollTrigger: {
+        trigger: ".Aiimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".Aicontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".Aicontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".successcontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".successcontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".successcards", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".successcards",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".pricingcard3", {
+      x: 500,
+      duration: 1.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard3",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".pricingcard1", {
+      x: -500,
+      duration: 1.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard1",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".pricingcard2", {
+      y: -100,
+      duration: 1.5,
+      delay: 1,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard2",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".faqcontent", {
+      y: -100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".faqcontent",
+        scroll: "body",
+        start: "top 90%",
+      },
+    });
+    gsap.from(".faqcontent2", {
+      y: -100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".faqcontent2",
+        scroll: "body",
+        start: "top 90%",
+      },
+    });
   });
 
-  gsap.from(".heading", {
-    y: 200,
-    opacity: 0,
-    duration: 1,
-    scale: 0.2,
-    scrollTrigger: {
-      trigger: ".heading",
-      start: "top 90%",
-      scroller: "body",
-       
-    },
-  });
+  mm.add("(max-width: 768px)", () => {
+    gsap.from(".imageslist", {
+      y: 20,
+      opacity: 0.6,
+      duration: 0.5,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: ".imageslist",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
 
-  gsap.from(".before-image", {
-    y: 50,
-    duration: 2,
-    delay: 0.1,
-    scrollTrigger: {
-      trigger: ".before-image",
-      start: "top 90%",
-      scroller: "body",
-    },
-  });
+    gsap.from(".heading", {
+      y: 200,
+      opacity: 0,
+      duration: 1,
+      scale: 0.2,
+      scrollTrigger: {
+        trigger: ".heading",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
 
-  gsap.from(".image", {
-    opacity: 0,
-    duration: 2,
-    x: 500,
-    scrollTrigger: {
-      trigger: ".image",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".before-image", {
+      y: 50,
+      duration: 2,
+      delay: 0.1,
+      scrollTrigger: {
+        trigger: ".before-image",
+        start: "top 90%",
+        scroller: "body",
+      },
+    });
 
-  gsap.from(".preciseimage", {
-    opacity: 0,
-    duration: 2,
-    x: 500,
-    scrollTrigger: {
-      trigger: ".preciseimage",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".image", {
+      opacity: 0,
+      duration: 2,
+      y: 100,
+      scrollTrigger: {
+        trigger: ".image",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".precisecontent", {
-    y: 20,
-    opacity: 0,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".precisecontent",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".preciseimage", {
+      opacity: 0,
+      duration: 2,
+      y: 100,
+      scrollTrigger: {
+        trigger: ".preciseimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".analyticsimage", {
-    opacity: 0,
-    duration: 2,
-    x: -500,
-    scrollTrigger: {
-      trigger: ".analyticsimage",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".precisecontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".precisecontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".analyticscontent", {
-    y: 20,
-    opacity: 0,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".analyticscontent",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".analyticsimage", {
+      opacity: 0,
+      duration: 2,
+      y: 100,
+      scrollTrigger: {
+        trigger: ".analyticsimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".Integratedcontent", {
-    y: 20,
-    opacity: 0,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".Integratedcontent",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".analyticscontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".analyticscontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".Aiimage", {
-    opacity: 0,
-    duration: 2,
-    x: 500,
-    scrollTrigger: {
-      trigger: ".Aiimage",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".Integratedcontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".Integratedcontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".Aicontent", {
-    y: 20,
-    opacity: 0,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".Aicontent",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".Aiimage", {
+      opacity: 0,
+      duration: 2,
+      y: 100,
+      scrollTrigger: {
+        trigger: ".Aiimage",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".successcontent", {
-    y: 20,
-    opacity: 0,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".successcontent",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".Aicontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".Aicontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".successcards", {
-    y: 100,
-    duration: 1,
-    opacity: 0,
-    stagger: 0.5,
-    scrollTrigger: {
-      trigger: ".successcards",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".successcontent", {
+      y: 20,
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: ".successcontent",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".pricingcard3", {
-    x: 500,
-    duration: 1.5,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".pricingcard3",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".successcards", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".successcards",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".pricingcard1", {
-    x: -500,
-    duration: 1.5,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".pricingcard1",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".pricingcard3", {
+      y: 100,
+      duration: 1.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard3",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".pricingcard2", {
-    y: -100,
-    duration: 1.5,
-    delay: 1,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".pricingcard2",
-      scroller: "body",
-      start: "top 90%",
-    },
-  });
+    gsap.from(".pricingcard1", {
+      y: 100,
+      duration: 1.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard1",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
 
-  gsap.from(".faqcontent", {
-    y: -100,
-    duration: 1,
-    opacity: 0,
-    stagger: 1,
-    scrollTrigger: {
-      trigger: ".faqcontent",
-      scroll: "body",
-      start: "top 90%",
-    },
-  });
-  gsap.from(".faqcontent2", {
-    y: -100,
-    duration: 1,
-    opacity: 0,
-    stagger: 1,
-    scrollTrigger: {
-      trigger: ".faqcontent2",
-      scroll: "body",
-      start: "top 90%",
-    },
+    gsap.from(".pricingcard2", {
+      y: 100,
+      duration: 1.5,
+      delay: 1,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".pricingcard2",
+        scroller: "body",
+        start: "top 90%",
+      },
+    });
+
+    gsap.from(".faqcontent", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".faqcontent",
+        scroll: "body",
+        start: "top 90%",
+      },
+    });
+    gsap.from(".faqcontent2", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".faqcontent2",
+        scroll: "body",
+        start: "top 90%",
+      },
+    });
   });
 });
